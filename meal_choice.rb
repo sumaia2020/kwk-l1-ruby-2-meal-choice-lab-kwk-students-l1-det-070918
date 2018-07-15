e2e6236cb075c5f3ca24ff54997c274d19487de2
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-15 15:03:27 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -23,13 +23,13 @@ end
 
 
 
-# # Call the methods with puts and your own arguments here. Like this:
-# puts snacks("Swedish fish")
-# ## This returns "Any time, is the right time for Sweedish fish!"
+# Call the methods with puts and your own arguments here. Like this:
+puts snacks("Swedish fish")
+## This returns "Any time, is the right time for Sweedish fish!"
 
-# # call your methods here
+# call your methods here
 
 
-# # Call your methods without any arguments here
-# puts snacks
-# ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
+# Call your methods without any arguments here
+puts snacks
+## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
